cf5c3874aaf953745715faf5ff071bb485464867
oddonenso@gmail.com <oddonenso@gmail.com>
2025-05-14 11:02:29 +0700

    migrate ORM

---
+++ database/migrations/2025_05_14_032001_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('completed_at')->nullable();
+            $table->unsignedBigInteger('warehouse_id');
+            $table->string('status'); // "active", "completed", "canceled"
+            $table->foreign('warehouse_id')->references('id')->on('warehouses');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
